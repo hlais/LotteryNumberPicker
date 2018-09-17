8834393226c53b62bb4237c42468bab73fdde59e
Halim Lais <halimlais@hotmail.com>
2018-09-17 11:58:43 +0100

    Lottery Number Picker
    Visual Studios lottery number generator, with  algorithm

--- LotteryNumberPicker/Form1.Designer.cs
+++ LotteryNumberPicker/Form1.Designer.cs
@@ -28,13 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.GenerateBtn = new System.Windows.Forms.Button();
+            this.ClearBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(22, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(248, 166);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // GenerateBtn
+            // 
+            this.GenerateBtn.Location = new System.Drawing.Point(22, 210);
+            this.GenerateBtn.Name = "GenerateBtn";
+            this.GenerateBtn.Size = new System.Drawing.Size(171, 23);
+            this.GenerateBtn.TabIndex = 1;
+            this.GenerateBtn.Text = "Generate Numbers";
+            this.GenerateBtn.UseVisualStyleBackColor = true;
+            this.GenerateBtn.Click += new System.EventHandler(this.generate_Click);
+            // 
+            // ClearBtn
+            // 
+            this.ClearBtn.Location = new System.Drawing.Point(199, 210);
+            this.ClearBtn.Name = "ClearBtn";
+            this.ClearBtn.Size = new System.Drawing.Size(71, 23);
+            this.ClearBtn.TabIndex = 2;
+            this.ClearBtn.Text = "Clear";
+            this.ClearBtn.UseVisualStyleBackColor = true;
+            this.ClearBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 258);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(124, 258);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(235, 258);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 289);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(124, 289);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(235, 289);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(295, 344);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ClearBtn);
+            this.Controls.Add(this.GenerateBtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Lottery Number Picker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button GenerateBtn;
+        private System.Windows.Forms.Button ClearBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- LotteryNumberPicker/Form1.cs
+++ LotteryNumberPicker/Form1.cs
@@ -12,9 +12,91 @@ namespace LotteryNumberPicker
 {
     public partial class Form1 : Form
     {
+        Random ran = new Random();
+        int[] numbers = new int[6];
+       
+
         public Form1()
         {
             InitializeComponent();
+            
+        }
+
+        private void generate_Click(object sender, EventArgs e)
+        {
+            //array to hold numbers
+            int[] seq = new int[59];
+            
+            //fill array elements 0-59
+            for (int i = 1; i < 60; i++)
+            {
+                seq[i - 1] = i;
+            }
+            //shuffle values in array
+
+            //sweet algorithm
+            for (int i = 0; i < 59; i++)
+            {
+                int randomNum = (ran.Next()%59);
+                int k = seq[i];
+                seq[i] = seq[randomNum];
+                seq[randomNum] = k;
+            }
+
+           
+            label1.Text = seq[1].ToString();
+            label2.Text = seq[2].ToString();
+            label3.Text = seq[3].ToString();
+            label4.Text = seq[4].ToString();
+            label5.Text = seq[5].ToString();
+            label6.Text = seq[6].ToString();
+            ClearBtn.Enabled = true;
+            GenerateBtn.Enabled = false;
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    int randomNumber = ran.Next(0,57);
+            //    numbers[i] = ran.Next(0, 57);
+            //    if (numbers.Contains(randomNumber))
+            //    {
+
+            //    }
+            //}
+            //ShowNumbers();
+        }
+        public void ShowNumbers()
+        {
+            //no longer in use- For demo purpose.
+            label1.Text = numbers[0].ToString();
+            label2.Text = numbers[1].ToString();
+            label3.Text = numbers[2].ToString();
+            label4.Text = numbers[3].ToString();
+            label5.Text = numbers[4].ToString();
+            label6.Text = numbers[5].ToString();
+            ClearBtn.Enabled = true;
+            GenerateBtn.Enabled = false;
+
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Clear();
+        }
+        private void Clear()
+        {
+            Array.Clear(numbers, 0, numbers.Length);
+            foreach (Label lbl in Controls.OfType<Label>())
+            {
+                lbl.Text = "0";
+            }
+            GenerateBtn.Enabled = true;
+            ClearBtn.Enabled = false;
         }
     }
 }
